0813800850d2370eaa70358824ee3f0bca440b40
ZeroSHiiFT <bling435@gmail.com>
2016-08-24 17:55:39 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+z=input("Enter a adjective:")
+x=input("Enter a noun:")
+c=input("Enter a plural noun")
+v=input("Enter a adjective:")
+b=input("Enter a adjective:")
+n=input("Enter a \"ing\" verb")
+m=input("Enter a \"ing\" verb")
+
+q=("If you go to some" + z + "place like Yellowstone National" + x)
+w=("," + " you must know how to deal with wild animals such as bears and wolves and" + c + ".")
+e=("The most important of these is the bear. There are three kinds of bear, the grizzly bear, the " + v)
+r=("bear, and the " + b + "bear. Bears pend most of their time " + n +" or " + m + ".")
+
+
+
+print(q+w+e+r)
